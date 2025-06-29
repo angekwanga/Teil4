2fa580fef37c95b4674a968ac687c849f6a40fda
ankw4339 <ankw4339@bht-berlin.de>
2025-06-29 11:59:09 +0200

    initial main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
+#include "network.h"
+#include "scheduled_trip.h"
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char **argv) {
+  bht::Network n{argv[1]};
+  bht::NetworkScheduledTrip trip = n.getScheduledTrip("230419258");
+  for (bht::NetworkScheduledTrip::iterator iter = trip.begin(); iter != trip.end(); iter++) {
+    std::cout << iter->stopSequence << ": " << n.stops[iter->stopId].name << std::endl;
+  }
 }
